5b2a59c1c3daed0afd96ad7d3455f7e24d2181e7
Hussein <hjundi@myseneca.ca>
2024-10-05 08:59:08 -0400

    Added lab3a

---
+++ lab3a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+#return_text_value() function
+
+#Author ID: hjundi
+
+def return_text_value():
+    return 'Good Morning Terry'
+    
+    
+#return_text_value() function
+
+def return_number_value():
+    num1 = 6
+    num2 = 9
+    num3 = num1 + num2
+    return num3
+
+
+
+#Main Program
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
